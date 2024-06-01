acd55515a9bd1b4e1c0ba47f3f763d3b6c14f89e
choi-mk <xhrlmin01@dgist.ac.kr>
2024-06-01 21:37:09 +0900

    test

--- sim.py
+++ sim.py
@@ -26,24 +26,21 @@ action
 """
 class DateSimulation:
     def __init__(self, place, start_img, scale=(20, 15), roi = ((120,50), (500, 420))) -> None:
-        self._good_ending   = cv2.imread("images/good_ending.png", cv2.IMREAD_COLOR)
-        self._bad_ending    = cv2.imread("images/bad_ending2.png", cv2.IMREAD_COLOR)
-        self._normal_ending = cv2.imread("images/normal_ending.png", cv2.IMREAD_COLOR)
         self._start_x, self._start_y = place[0], place[1]
         self._width, self._height = place[2]-place[0], place[3]-place[1]
         self._start_img=start_img
-        
+          
         
         self._roi = roi
-        self._roi_start_x, self._roi_start_y = self._roi[0]
+        self._roi_start_x, self._roi_start_y = self._roi[0][0]+place[0], self._roi[0][1]+place[1]
         self._roi_width, self._roi_height = roi[1][0] - roi[0][0], roi[1][1] - roi[0][1] #map(int, resolution.split('x'))
         
         self._scale = scale
-        self._threshold = 0.0001
-        self._sim_checker = utill.SimilarityChecker(self._normal_ending, self._bad_ending, self._threshold)
+        self._threshold = 0.001
+        #self._sim_checker = utill.SimilarityChecker(self._normal_ending, self._bad_ending, self._threshold)
     
     def _command_to_pixel(self, x, y):
-        return x/self._scale[0]*self._roi_width + self._roi_start_x, y/self._scale[1]*self._roi_height + self._roi_start_y
+        return x+self._start_x, y+self._start_y #x/self._scale[0]*self._roi_width + self._roi_start_x, y/self._scale[1]*self._roi_height + self._roi_start_y
 
     def get_image(self):
         return ImageGrab.grab((self._start_x, self._start_y, self._start_x+self._width, self._start_y + self._height))
@@ -61,54 +58,40 @@ class DateSimulation:
             pyautogui.click()
         elif type == "d":
             pyautogui.dragTo(x,y)
-
-        next_state, ending_type, immediate_ret =self._wait_for_stabilized()
-        reward= -1
-        if immediate_ret: #유사도 측정 방법 변경
+        time.sleep(1)
+        next_state=self.get_state()
+        reward=-1    
+        done=False
+        if state!=next_state: #유사도 측정 방법 변경
             reward=1
-
-        done= ending_type != 0
-        # type = self._sim_checker.ending_check(next_state)
-        # if next_state == self._start_img: #유사도
-        #     print(1)
-        #     done=True
-        #     reward=utill.cal_ending(state)
-        if ending_type == 1:
-            reward = 10
-
-        elif ending_type == 2:
-            retward = 20
-
+            done, next_state=self._wait_for_stabilized()
+        #type = self._sim_checker.ending_check(next_state)
+        if done: #유사도
+            reward=utill.cal_ending(next_state)
         return  next_state, reward, done
 
     def _wait_for_stabilized(self):
-        wait_time = 0.1
         prev_state = self.get_state()
-        time.sleep(wait_time)
-        self._try_to_skip(5)
-        current_state = self.get_state()
-        b, v = self._sim_checker.check(prev_state, current_state)
-        print(f"Stabilizing similarity result: {v}")
-        if b:
-            return current_state, False, False
         while True:
-            time.sleep(wait_time)
             self._try_to_skip()
+            time.sleep(8)
             current_state = self.get_state()
-            b, v = self._sim_checker.check(prev_state, current_state)
-            print(f"Stabilizing similarity result: {v}")
-            if b:
-                eb = self._sim_checker.ending_check(current_state)
-                return current_state, eb, True
+            if current_state==self._start_img:
+                print(1)
+                 
+                return True, prev_state
+            # print(f"Stabilizing simil     arity result: {v}")
+            if prev_state==current_state:
+                return False, current_state
             #self._try_to_skip()
             prev_state = current_state
     
-    def _try_to_skip(self, cnt = 5):
+    def _try_to_skip(self, cnt = 6):
         for i in range(cnt):
             pyautogui.press("space")
-
-    def get_reward(self):
-        return 0.0, False
+   
+    #def get_reward(self):
+        #return 0.0, False
 
     def reset(self):
         sleep_time = 0.4
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import pyautogui
 import time
 import matplotlib.pyplot as plt
 import utill
-import numpy as np
+import numpy as np 
 
 def play_and_train(env, agent, t_max=1000):
     """
@@ -23,13 +23,10 @@ def play_and_train(env, agent, t_max=1000):
         #     pyautogui.press('enter')
         #     time.sleep(0.2)
         next_s = s      
-        # while r==0:
-        start_time = time.time()
-        a = agent.get_action(s)
-        print(f"Do Action: {a}")
-
-        next_s, r, done= env.step(s, a)
-        print(f"Reward: {r} Feedback time {time.time() - start_time}")
+        while r==0:       
+            a = agent.get_action(s)
+            next_s, r, done= env.step(s, a)
+            print(f"Action {a}, and reward {r}")
 
         # train (update) agent for state s
         agent.update(s, a,r,next_s)
@@ -42,28 +39,29 @@ def play_and_train(env, agent, t_max=1000):
     return total_reward
 
 if __name__ == "__main__":
-    model_path = "data/test.pkl"
     actions = []
     for i in range(20):
-        for j in range(15):
+        for j  in range(15):          
             for t in ["c"]:     
                 actions.append((i,j,t))
     place, start_img= utill.reset_position() 
+    #start_img.crop((250,100,450,300)).show()
     start_arr=np.array(start_img)
     start_tuple=utill.numpy2tuple(start_arr)
     env = DateSimulation(place, start_tuple)
+    actions=[(519, 350,'c'), (361, 351, 'c'), (516, 374,'c'), (160, 316, 'c'), (333, 124, 'c'), (133, 75,'c'), (135, 26, 'c'), (344, 200, 'c'), (302, 261, 'c'), (396, 269, 'c'), (516, 316, 'c')]  #수정
+
     
-    agent = QLearningAgent(model_path, actions, alpha=0.5, epsilon=0.7, discount=0.99)
-    agent.load_qvalues()
-    #play_and_train(env=env, agent=agent)
-    #env.reset()
+    #actions.append(( 16, 14, 'c'))
+    #print(actions)
+
+    agent = QLearningAgent(actions, alpha=0.5, epsilon=0.7, discount=0.99)
+
     rewards = []
     for i in range(1000):
         rewards.append(play_and_train(env, agent))
-        agent.epsilon *= 0.9 #값 수정함a
-        agent.save_qvalues()
+        agent.epsilon *= 0.95 #값 수정함
         print(rewards[-1])
-        if True:
-            plt.title('eps = {:e}, mean reward = {:.1f}'.format(agent.epsilon, np.mean(rewards[-10:])))
-            plt.plot(rewards)
-            plt.show()
+    plt.title('eps = {:e}, mean reward = {:.1f}'.format(agent.epsilon, np.mean(rewards[-10:])))
+    plt.plot(rewards)
+    plt.show()
